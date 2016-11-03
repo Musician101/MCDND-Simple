644feeb62bb037c253e4c21c70fc6352c5a8e0ff
Musician101 <bruceabivens@gmail.com>
2016-11-03 02:57:23 -0700

    Read and write CharacterSheet to files.

--- mcdndsimple-sponge/src/main/java/io/musician101/mcdndsimple/sponge/SpongeMCDNDSimple.java
+++ mcdndsimple-sponge/src/main/java/io/musician101/mcdndsimple/sponge/SpongeMCDNDSimple.java
@@ -1,13 +1,17 @@
 package io.musician101.mcdndsimple.sponge;
 
+import io.musician101.mcdndsimple.sponge.character.CharacterSheetStorage;
 import io.musician101.musicianlibrary.java.minecraft.AbstractConfig;
 import io.musician101.musicianlibrary.java.minecraft.sponge.AbstractSpongePlugin;
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
+
+import java.io.File;
 
 @Plugin(
         id = "mcdndsimple",
@@ -19,10 +23,22 @@ import org.spongepowered.api.plugin.PluginContainer;
 )
 public class SpongeMCDNDSimple extends AbstractSpongePlugin<AbstractConfig>
 {
+    private static CharacterSheetStorage characterSheetStorage;
+
+    public static CharacterSheetStorage getCharacterSheetStorage()
+    {
+        return characterSheetStorage;
+    }
+
     @Override
     public void preInit(GamePreInitializationEvent event)
     {
+        characterSheetStorage = new CharacterSheetStorage(new File("mods/mcdndsimple/players"));
+    }
 
+    public void serverStop(GameStoppingServerEvent event)
+    {
+        characterSheetStorage.save();
     }
 
     @Override
